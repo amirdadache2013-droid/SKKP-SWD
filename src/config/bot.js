3fd66a15383533e94518c973cc6895a6eed9162a
amirdadache2013-droid <amir.dadache2013@gmail.com>
2026-06-10 00:43:15 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with polskarp1",
+        name: "Stworzony Przez polskarp1",
         // Activity type number (0 = Playing).
         type: 0, 
       },
