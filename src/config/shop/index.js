55d318feca78587e0dda0194430992d5e77a86e8
amirdadache2013-droid <amir.dadache2013@gmail.com>
2026-06-10 01:38:00 +0200

    Update index.js

--- src/config/shop/index.js
+++ src/config/shop/index.js
@@ -1,7 +1,1 @@
-import { loadCommands } from "./src/handlers/commandHandler.js";
 
-client.on("ready", () => {
-  loadCommands(client);
-  console.log("SWD BOT ONLINE 🚒");
-});
-client.commands = new Map();
