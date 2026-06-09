b6ab2de6f6a1d26ce8506315204bf9c3e299b816
amirdadache2013-droid <amir.dadache2013@gmail.com>
2026-06-10 01:33:22 +0200

    Update commandHandler.js

--- src/handlers/commandHandler.js
+++ src/handlers/commandHandler.js
@@ -1,1 +1,20 @@
+import fs from "fs";
+import path from "path";
 
+export function loadCommands(client) {
+  const commandsPath = path.resolve("./commands/swd");
+
+  const commandFiles = fs.readdirSync(commandsPath)
+    .filter(file => file.endsWith(".js"));
+
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = (await import(filePath)).default;
+
+    if (!command?.name || !command?.execute) continue;
+
+    client.commands.set(command.name, command);
+  }
+
+  console.log(`🚒 SWD: załadowano ${commandFiles.length} komend`);
+}
