6e02e7b1fd10ddb6e51b4d54fb38d3d2713abab5
amirdadache2013-droid <amir.dadache2013@gmail.com>
2026-06-10 02:21:24 +0200

    Update wyjazd.js

--- src/commands/swd/wyjazd.js
+++ src/commands/swd/wyjazd.js
@@ -1,1 +1,11 @@
+import { SlashCommandBuilder } from 'discord.js';
 
+export default {
+  data: new SlashCommandBuilder()
+    .setName('wyjazd')
+    .setDescription('Zgłoszenie wyjazdu jednostki SWD'),
+
+  async execute(interaction) {
+    await interaction.reply('🚨 Jednostka została zadysponowana (SWD RP)');
+  }
+};
